45b17a2d573237a71ed74c8036c67e771f506d73
catapillie <espritox@gmail.com>
2023-02-10 13:58:02 +0100

    don't wrap expression ast nodes in box

--- src/parsing/parser_ast.rs
+++ src/parsing/parser_ast.rs
@@ -2,11 +2,11 @@ use super::expression::Expression;
 
 #[derive(Debug)]
 pub enum ParserAST<'a>{
-    Expression(Box<Expression<'a>>)
+    Expression(Expression<'a>)
 }
 
 impl<'a> From<Expression<'a>> for ParserAST<'a> {
     fn from(val: Expression<'a>) -> Self {
-        ParserAST::Expression(Box::new(val))
+        ParserAST::Expression(val)
     }
 } 
